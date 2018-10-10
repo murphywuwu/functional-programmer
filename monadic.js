946810e9d262604429650c57241b5f7baf42199d
murphywuwu <huangxiulan_cd@keruyun.com>
2018-10-10 18:02:25 +0800

    up

--- monadic.js
+++ monadic.js
@@ -61,15 +61,51 @@ const result2 = catFirstClass('.git/config');
 console.log('result1', result1.$value().$value());
 console.log('result2', result2.$value().$value());
 
+/*
+
+init =>  Maybe.of([]);
+map  =>  Maybe.of(map(safeHead, []))
+              => Maybe.of({...})
+
+map  =>  Maybe.of(map(safeProp('street'), Maybe.of({...})))
+              => Maybe.of(safeProp('street'), {...})
+                      => Maybe.of({...})
+result: Maybe.of(Maybe.of(Maybe.of({ name: 'Mulburry', number: 8402 })))
+*/
 const safeProp = curry((x, obj) => Maybe.of(obj[x]));
 const safeHead = safeProp(0);
 
-const firstAddressStreet = compose(map(map(safeProp('street'))), map(safeHead), safeProp('address'));
+var firstAddressStreet = compose(map(map(safeProp('street'))), map(safeHead), safeProp('address'));
 
-const address = firstAddressStreet({
+var address = firstAddressStreet({
   address: [{ street: { name: 'Mulburry', number: 8402 }, postcode: 'WC2N' }]
 })
 
 const mmo = Maybe.of(Maybe.of('nunchucks'));
 
 mmo.join();
+
+// join :: Monad m => m (m a) => m a
+var join = function (mma) {
+  return mma.join();
+}
+
+/* 
+  safeProp('street'): 初始化,1
+  map(safeHead): 加一层,2
+  join: 去一层,1
+  map(safeProp('street')): 加一层,2
+  join: 去一层,1
+
+  result: Maybe.of({name: 'Mulburry', number: 8402 })
+ */
+var firstAddressStreet = compose(join, map(safeProp('street')) ,join, map(safeHead), safeProp('address'));
+
+var address = firstAddressStreet({
+  address: [
+    {
+      street: { name: 'Mulburry', number: 8402 },
+      postcode: 'WC2N' 
+    }
+  ]
+})
